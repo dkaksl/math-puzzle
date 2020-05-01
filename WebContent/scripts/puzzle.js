5163ce15736940971a4bc8a61ca6c85d1a342e5b
Amani Kilumanga <amani.kilumanga@gmail.com>
2020-05-01 17:55:39 +0200

    implemented solver; implemented answer and eval buttons

--- WebContent/scripts/puzzle.js
+++ WebContent/scripts/puzzle.js
@@ -14,12 +14,89 @@ $(document).ready(function () {
 		});
 	});
 	$("#randomButton").click(function () {
-		$(".number:not(#hardCoded)").text(function () {
-			return Math.floor(Math.random() * 10) + 1;
-		});
+		var variables = "abcdefgh".split("");
+		for (var i = 0; i < variables.length; i++) {
+			document.getElementById(variables[i]).value = Math.floor(Math.random() * 10) + 1;
+		}
+	});
+	$("#answerButton").click(function () {
+		var solutions = getSolutions();
+		var randomSolution = solutions[Math.floor(Math.random() * solutions.length)]
+		var variables = "abcdefgh".split("");
+		for (var i = 0; i < variables.length; i++) {
+			document.getElementById(variables[i]).value = randomSolution[i];
+		}
+
 	});
 	$("#evaluateButton").click(function () {
-		alert('you pressed the evaluate button');
+		var a = parseInt(document.getElementById('a').value)
+		var b = parseInt(document.getElementById('b').value)
+		var c = parseInt(document.getElementById('c').value)
+		var d = parseInt(document.getElementById('d').value)
+		var e = parseInt(document.getElementById('e').value)
+		var f = parseInt(document.getElementById('f').value)
+		var g = parseInt(document.getElementById('g').value)
+		var h = parseInt(document.getElementById('h').value)
+		var solved = isSolved(a, b, c, d, e, f, g, h)
+		alert((solved) ? "all equations evaluate as true" : "not all equations evaluate as true")
 	});
 
 });
+
+function isSolved(a, b, c, d, e, f, g, h) {
+	if (a + b !== 13) {
+		return false;
+	}
+
+	if ((c - d) * e !== 4) {
+		return false;
+	}
+
+	if (f + g - h !== 4) {
+		return false;
+	}
+
+	if (a + c !== 4 * f) {
+		return false;
+	}
+
+	if ((b - d) * g !== 4) {
+		return false;
+	}
+
+	if (e + h !== 5) {
+		return false;
+	}
+
+	return true;
+}
+
+function getSolutions() {
+	var solutions = []
+	var from = 0;
+	var to = 10;
+	var count = 50;
+	for (var a = from; a < to; a++) {
+		for (var b = from; b < to; b++) {
+			for (var c = from; c < to; c++) {
+				for (var d = from; d < to; d++) {
+					for (var e = from; e < to; e++) {
+						for (var f = from; f < to; f++) {
+							for (var g = from; g < to; g++) {
+								for (var h = from; h < to; h++) {
+									if (isSolved(a, b, c, d, e, f, g, h)) {
+										solutions.push([a, b, c, d, e, f, g, h])
+										if (solutions.length >= count) {
+											return solutions;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return solutions;
+}
