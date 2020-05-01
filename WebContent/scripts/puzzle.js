daac86cae84fa748d2792809ec2509d88808081c
Amani Kilumanga <amani.kilumanga@gmail.com>
2020-05-01 15:20:54 +0200

    formatting; added eval button

--- WebContent/scripts/puzzle.js
+++ WebContent/scripts/puzzle.js
@@ -1,27 +1,25 @@
-$(document).ready(function() {
-	$(".answer").text(function() {
+$(document).ready(function () {
+	$(".answer").text(function () {
 		return "4";
 	});
-	$(".number:not(#hardCoded)").text(function() {
+	$(".number:not(#hardCoded)").text(function () {
 		return " ";
 	});
-	$("#hardCoded").text(function() {
+	$("#hardCoded").text(function () {
 		return 9;
 	});
-	$("#clearButton").click(function() {
-		$(".number:not(#hardCoded)").text(function() {
+	$("#clearButton").click(function () {
+		$(".number:not(#hardCoded)").text(function () {
 			return "";
 		});
 	});
-	$("#randomButton").click(function() {
-		$(".number:not(#hardCoded)").text(function() {
+	$("#randomButton").click(function () {
+		$(".number:not(#hardCoded)").text(function () {
 			return Math.floor(Math.random() * 10) + 1;
 		});
 	});
-	/*
-	 * $("#puzzle").width(function() { var parentWidth =
-	 * $("#puzzleContainer").width(); var puzzleWidth = (parentWidth * 0.7);
-	 * return puzzleWidth + "%"; });
-	 */
+	$("#evaluateButton").click(function () {
+		alert('you pressed the evaluate button');
+	});
 
 });
